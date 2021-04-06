6ae75f0f7c821cc59f2eada809ab2443411eca75
Christine Contreras <christineec23@yahoo.com>
2021-04-06 18:24:10 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+
+//function with default paramater value is roller-skate
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+//function expression 
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+};
+
+
+//function that's parameters can be used within anonymous function 
+function wrapAdjective(flair = "*") {
+    //return a function with default parameter special
+    return function(text = 'special') {
+        return `You are ${flair}${text}${flair}!`;
+    }
+}
